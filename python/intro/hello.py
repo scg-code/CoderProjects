7928714f6973e43e14685e8bbfc8cfd493264118
scg-code <samgifford.work@gmail.com>
2023-10-05 11:35:19 +1000

    python testing

--- python/intro/hello.py
+++ python/intro/hello.py
@@ -1,3 +1,4 @@
-print('What is your name?')
-my_name = input()
-print('Hello, ' + my_name)
+my_name = input('What is your name?' )
+my_age = int(input("What is your age?" ))
+# print('Hello, ' + my_name + ' you are ' + str(my_age + 10) + ' years old in 10 years.')
+print(f"Hello, {my_name} you are {my_age + 10} years old in 10 years.")
