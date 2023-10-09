373b162c712a52babbb9219e2d4ba589f4581200
scg-code <samgifford.work@gmail.com>
2023-10-10 08:52:01 +1000

    some loops practice

---
+++ python/functions/challenge.py
@@ -0,0 +1,43 @@
+# Write a function to calculate the score for one "frame" of ten-pin bowling. An entire game consists of 10 frames, with up to 2 balls bowled per frame.
+
+# Due to how the scoring works, you will need to know how many pins were knocked down by the last 3 balls bowled in order to calculate the total score for the frame.
+
+# The scoring rules are:
+# 1 point for each pin knocked down (so between 0 and 10)
+# If it's a strike (all 10 pins knocked down with 1 ball), add the total of the next 2 balls
+# If it's a spare (all 10 pins knocked down with 2 balls), add the next ball
+
+# Example calls with output:
+
+# calc_frame(6, 2, 9)     # 8
+# calc_frame(10, 6, 2)    # 18
+# calc_frame(7, 3, 4)     # 14
+# calc_frame(0, 10, 5)    # 15
+# calc_frame(5, 0, 10)    # 5
+
+
+
+
+
+def calc_frame(ball1, ball2, ball3):
+    # First, let's calculate the total score for this frame.
+    frame_score = ball1 + ball2 + ball3
+
+    # If it's a strike (all pins knocked down with the first ball),
+    if ball1 == 10:
+        # Add the total of the next two balls to the frame_score.
+        frame_score += ball2 + ball3
+    # If it's not a strike but a spare (all pins knocked down with the first two balls),
+    elif ball1 + ball2 == 10:
+        # Add the next ball's score to the frame_score.
+        frame_score += ball3
+
+    # Return the total score for this frame.
+    return frame_score
+
+# Example calls with output:
+print(calc_frame(6, 2, 9))   # Output: 8
+print(calc_frame(10, 6, 2))  # Output: 18
+print(calc_frame(7, 3, 4))   # Output: 14
+print(calc_frame(0, 10, 5))  # Output: 15
+print(calc_frame(5, 0, 10))  # Output: 5
--- python/functions/functions.py
+++ python/functions/functions.py
@@ -30,64 +30,116 @@
 # Coding Score Calculator
 
 # Initialise objects to be used.
-coding_skills_dict = {
-    "Python": 1,
-    "Ruby": 2,
-    "Bash": 4,
-    "Git": 8,
-    "HTML": 16,
-    "TDD": 32,
-    "CSS": 64,
-    "JavaScript": 128
-}
-skill_check = "" # String variable to take 'y' or 'n' input from user for each skill.
-user_skills = [] # List containing booleans corresponding to each coding skill.
-total_score = 0 # Total score of all their skills added together.
+# coding_skills_dict = {
+#     "Python": 1,
+#     "Ruby": 2,
+#     "Bash": 4,
+#     "Git": 8,
+#     "HTML": 16,
+#     "TDD": 32,
+#     "CSS": 64,
+#     "JavaScript": 128
+# }
+# skill_check = "" # String variable to take 'y' or 'n' input from user for each skill.
+# user_skills = [] # List containing booleans corresponding to each coding skill.
+# total_score = 0 # Total score of all their skills added together.
 
-# Welcoming splash printed to console.
-print("")
-print("------------------------------")
-print("-----WELCOME TO ACME CORP-----")
-print("------------------------------")
-print("")
-print("Coding Skill Calculator")
-print("")
+# # Welcoming splash printed to console.
+# print("")
+# print("------------------------------")
+# print("-----WELCOME TO ACME CORP-----")
+# print("------------------------------")
+# print("")
+# print("Coding Skill Calculator")
+# print("")
 
-# Iterate over all keys in the coding_skills_dict and get user input for each.
-for k in coding_skills_dict.keys():
-    # Use a while loop in case the user enters an invalid input. It will just ask the question again.
-    while skill_check != "y" and skill_check != "n":
-        skill_check = input(f"Are you competent with {k}? Enter 'y' or 'n': ")
-    # Add skill to the user_skills list as a boolean.
-    if skill_check == "y":
-        user_skills.append(True)
+# # Iterate over all keys in the coding_skills_dict and get user input for each.
+# for k in coding_skills_dict.keys():
+#     # Use a while loop in case the user enters an invalid input. It will just ask the question again.
+#     while skill_check != "y" and skill_check != "n":
+#         skill_check = input(f"Are you competent with {k}? Enter 'y' or 'n': ")
+#     # Add skill to the user_skills list as a boolean.
+#     if skill_check == "y":
+#         user_skills.append(True)
+#     else:
+#         user_skills.append(False)
+#     # Reset skill_check variable.
+#     skill_check = ""
+
+# # Calculate total skill score. All scores are powers of 2, so we can just use 2^i.
+# for i in range(len(user_skills)):
+#     total_score += user_skills[i] * (2 ** i)
+
+# # Provide feedback to the user showing their score and potential improvements.
+# print("")
+# print(f"Thank you for your input! Your total coding score is {total_score} out of a possible 255.")
+# print("")
+
+# # Offer a list of compentencies based on the users previous inputs.
+# if total_score < 255:
+#     print("To improve your score, you could learn the following skills:")
+#     i = 0
+#     for k in coding_skills_dict.keys():
+#         if not user_skills[i]:
+#             print(f"- {k} would improve your score by {coding_skills_dict[k]}.")
+#         i += 1
+
+# # If the user has already achieved a perfect score, congratulate them and offer no improvements
+# else:
+#     print("You have a perfect score. There are no other competencies that would improve your score! Bravo!")
+
+# # Let the user know the application has been closed.
+# print("")
+# print("Application Closed")
+
+
+# While Loop
+# i = 1
+# while i <= 10 :
+#     print(i)
+#     i += 1
+# print("Done with loop")
+
+
+
+# secret word game
+
+# secret_word = "reindeer"
+# #variable to store guess
+# guess = ""
+# guess_count = 0
+# guess_limit = 3
+# out_of_guesses = False
+
+# # While loop to continually ask until guessed correctly
+# while guess != secret_word and not (out_of_guesses):
+#     if guess_count < guess_limit:
+#         guess = input("Enter Guess: ")
+#         guess_count += 1
+#     else:
+#         out_of_guesses = True
+
+# if out_of_guesses:
+#     print("Out of guesses, YOU LOSE!")
+# else:
+#     print("You win!")
+
+
+# For loops array
+
+friends = ["Jim", "John", "Martha"]
+for name in friends:
+    print(name)
+
+friends = ["Jim", "John", "Martha"]
+for index in range(len(friends)):
+    print(friends[index])
+
+
+friends = ["Jim", "John", "Martha"]
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
     else:
-        user_skills.append(False)
-    # Reset skill_check variable.
-    skill_check = ""
-
-# Calculate total skill score. All scores are powers of 2, so we can just use 2^i.
-for i in range(len(user_skills)):
-    total_score += user_skills[i] * (2 ** i)
-
-# Provide feedback to the user showing their score and potential improvements.
-print("")
-print(f"Thank you for your input! Your total coding score is {total_score} out of a possible 255.")
-print("")
-
-# Offer a list of compentencies based on the users previous inputs.
-if total_score < 255:
-    print("To improve your score, you could learn the following skills:")
-    i = 0
-    for k in coding_skills_dict.keys():
-        if not user_skills[i]:
-            print(f"- {k} would improve your score by {coding_skills_dict[k]}.")
-        i += 1
-
-# If the user has already achieved a perfect score, congratulate them and offer no improvements
-else:
-    print("You have a perfect score. There are no other competencies that would improve your score! Bravo!")
-
-# Let the user know the application has been closed.
-print("")
-print("Application Closed")
+        print("not first")
