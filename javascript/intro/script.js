0103c601525d0b900d3b384c78fbe5c125dcb82e
Samuel Gifford <samuelgifford@Samuels-MacBook-Pro.local>
2023-12-19 13:36:28 +1000

    javascript intro

---
+++ javascript/intro/script.js
@@ -0,0 +1,43 @@
+let str = 'Hello World'
+
+// console.log(str.replaceAll('o', '---')) // Hell--- W---rld
+
+// Python: print(f'hello, {name}')
+// console.log(`hello, ${str}`) // hello, Hello World 
+// console.log(`Answer: ${2 + 2}`) // Answer: 4
+
+// console.log(Math.floor(3 / 2)) // 1.5 
+
+// let x = 5
+// console.log(++x)
+// // increment X (Python: x += 1)
+// // x++
+// console.log(x)
+
+// console.log(123 == 123)
+// console.log(123 === '123')
+
+// let x = 5
+
+// console.log(typeof x) // number
+
+// Python: person = {'name': 'John', 'age': 36}
+// let person = { 
+//     name: 'John',
+//     age: 36 
+// }
+
+// console.log(person.name) // John
+
+// x = [1, 2, 3, 4, 5, true, 'hello', [1, 2, 3]]
+
+// console.log(x)
+
+// Python
+// def add(x, y):
+//     return x + y
+function add(x, y) {
+    return x + y
+}
+
+console.log(add(2, 3)) // 5
