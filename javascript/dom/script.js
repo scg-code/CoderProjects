c9520b0aac06c5cd1c4a6222dbca12ecd7bf5734
Samuel Gifford <samuelgifford@Samuels-MacBook-Pro.local>
2024-01-15 18:46:05 +1000

    complete js dom

---
+++ javascript/dom/script.js
@@ -0,0 +1,9 @@
+// const newDiv = document.createElement("div");
+// console.log(newDiv);
+
+// newDiv.innerHTML = '<h2 style="color: blue;">Hello World</h2>';
+// newDiv.id = "hello";
+
+// document.body.insertBefore(newDiv, document.querySelector("ul"));
+// const myColor = 'blue';
+// document.body.innerHTML += `<div id="hello"> <h2 style="color: blue;">Hello World</h2> </div>`;
