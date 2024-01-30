157761f1d596453c2123c7467c1152c466af8408
Samuel Gifford <samuelgifford@Samuels-MacBook-Pro.local>
2024-01-30 12:41:52 +1000

    react states

---
+++ javascript/state/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import BitcoinIndex from "./BitcoinIndex";
+
+
+
+const App = () => {
+  
+  return (
+    <>
+      <h1>Bitcoin Index</h1>
+      <BitcoinIndex />
+    </>
+  );
+};
+
+export default App;
